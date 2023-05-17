ed5e8ab71cd02419a194fc3d5b30c4989e45cea4
Razan <razanalzannan@gmail.com>
2023-05-17 12:20:13 +0300

    Initial Commit

---
+++ LinkedIn/LinkedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInApp.swift
+//  LinkedIn
+//
+//  Created by Razan Mohammed Alzannan on 27/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
